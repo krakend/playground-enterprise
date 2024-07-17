a1b1a02c8f524033131cc0929d39fbd2ecc2b201
Shreyansh <shreyansh@codalent.com>
2024-07-17 13:15:55 +0530

    feat:basic setup

--- config/krakend/demo-redesign/src/app/layout.tsx
+++ config/krakend/demo-redesign/src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.scss";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
