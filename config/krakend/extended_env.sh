78ed120f8d8971c2eb818d6136fcda27a18e84c7
David Hontecillas <dhontecillas@gmail.com>
2024-07-17 16:42:34 +0200

    fix env script for running local krakend with flexible config

--- config/krakend/extended_env.sh
+++ config/krakend/extended_env.sh
@@ -7,10 +7,10 @@ export FC_CONFIG=$PWD/fc_config.local.json
 
 # this alias will run the krakend in a different port of that used by the dockerized krakend
 # from the docker-compose file:
-alias r='./krakend run --config ./extended/krakend.json --port 8666'
+alias r='./krakend run --config ./extended/krakend.tmpl --port 8666'
 
 # this alias allows to check the configuration and the issues it might have
-alias d='clear; ../../krakend check -c extended/krakend.json 2> out.err.dbg | tee out.dbg'
+alias d='clear; ../../krakend check -c extended/krakend.tmpl 2> out.err.dbg | tee out.dbg'
 
 # this alias provides info about the composed output file in case of error
 alias i='cat out.err.dbg | grep "ERROR parsing the configuration file" | sed "s/.*tmp\(.*\)json.*/\/tmp\/\1json/g" | xargs cat'
